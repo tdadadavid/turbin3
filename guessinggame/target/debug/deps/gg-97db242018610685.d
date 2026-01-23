7891acc26e9279d2b24ef8ca03aca6e9ffc5aae6
tdadadavid <davidtofunmidada@gmail.com>
2026-01-23 15:25:12 +0100

    fix: gitignore
    Signed-off-by: tdadadavid <davidtofunmidada@gmail.com>

--- guessinggame/target/debug/deps/gg-97db242018610685.d
+++
@@ -1,5 +0,0 @@
-/Users/king/Documents/personal/turbin3/gg/target/debug/deps/gg-97db242018610685.d: src/main.rs
-
-/Users/king/Documents/personal/turbin3/gg/target/debug/deps/gg-97db242018610685: src/main.rs
-
-src/main.rs:
--- guessinggame/target/debug/gg.d
+++
@@ -1,1 +0,0 @@
-/Users/king/Documents/personal/turbin3/gg/target/debug/gg: /Users/king/Documents/personal/turbin3/gg/src/main.rs
