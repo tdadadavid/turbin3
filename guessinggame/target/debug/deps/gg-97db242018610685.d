ad6810f402bacc13e87830b160a17f538652edb9
tdadadavid <davidtofunmidada@gmail.com>
2026-01-23 15:23:33 +0100

    fix: change name for easy work
    Signed-off-by: tdadadavid <davidtofunmidada@gmail.com>

---
+++ guessinggame/target/debug/deps/gg-97db242018610685.d
@@ -0,0 +1,5 @@
+/Users/king/Documents/personal/turbin3/gg/target/debug/deps/gg-97db242018610685.d: src/main.rs
+
+/Users/king/Documents/personal/turbin3/gg/target/debug/deps/gg-97db242018610685: src/main.rs
+
+src/main.rs:
---
+++ guessinggame/target/debug/gg.d
@@ -0,0 +1,1 @@
+/Users/king/Documents/personal/turbin3/gg/target/debug/gg: /Users/king/Documents/personal/turbin3/gg/src/main.rs
